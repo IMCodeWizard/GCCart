21f74ee7fd3d14dfaaa9f06ab4abb389e78cf52d
iOS Ninja <dev.igormak@gmail.com>
2019-02-21 18:42:50 +0200

    Search feature added

--- GCCart/GCCart/View Controllers/CartDetailsViewController.swift
+++ GCCart/GCCart/View Controllers/CartDetailsViewController.swift
@@ -8,24 +8,20 @@
 
 import UIKit
 
-class CartDetailsViewController: BaseViewController {
+class CartDetailsViewController: UIViewController {
     
     @IBOutlet weak var loadingStackView: UIStackView!
     @IBOutlet weak var tableView: UITableView!
     
     private var collectionData: [OrderItemViewModel]?
+    private var filteredCollectionData: [OrderItemViewModel]?
+    private var isSearching = false
     
     //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         fetchCollectionData()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,13 +34,28 @@ class CartDetailsViewController: BaseViewController {
     
     //MARK: - UI Methods
     func setupNavigatoinData(cartInfoData: CartInfoData) {
-        if let navItem = self.navigationController?.navigationItem {
-            self.title = "Total: \(cartInfoData.total.formattedPrice())"
-        }
-       
         
+        let title = "Total: \(cartInfoData.total.formattedPrice())"
+        
+
+
+        
+        if #available(iOS 11.0, *) {
+            navigationItem.title = title
+            
+            let searchController = UISearchController(searchResultsController: nil)
+            searchController.hidesNavigationBarDuringPresentation = true
+            searchController.dimsBackgroundDuringPresentation = false
+            searchController.searchResultsUpdater = self
+            navigationItem.searchController = searchController
+        } else {
+            // Fallback on earlier versions
+            navigationItem.prompt = "Cart " + title
+            let searchBar = UISearchBar()
+            searchBar.delegate = self
+            navigationItem.titleView = searchBar
+        }
     }
-    
     
     func showLoader(){
         self.loadingStackView.alpha = 0.0
@@ -60,8 +71,8 @@ class CartDetailsViewController: BaseViewController {
         })
     }
     
-    //MARK: - Custom Methods
     
+    //MARK: - Custom Methods
     func fetchCollectionData() {
         NetworkService.sharedService.sendRequestWithService(api: API.getCartItems(), success: { [weak self] (cartInfo: CartInfoData) in
             cartInfo.orderItemsInformation.forEach({print($0.product.name)})
@@ -83,21 +94,21 @@ extension CartDetailsViewController: UITableViewDataSource, UITableViewDelegate 
     
     /** Data Source */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.collectionData?.count ?? 0
+        return isSearching ? filteredCollectionData?.count ?? 0 : self.collectionData?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomCell
         
-        let orderItem = collectionData![indexPath.row]
+        let orderItem = isSearching ? filteredCollectionData![indexPath.row] : collectionData![indexPath.row]
         
         cell.bindData(orderItem: orderItem)
         
         return cell
     }
     
-    
+    /** Appearance */
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
     
         cell.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
@@ -124,5 +135,32 @@ extension CartDetailsViewController: UITableViewDataSource, UITableViewDelegate 
         }
     }
     
+}
+
+extension CartDetailsViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterCollectionData(by: searchController.searchBar.text)
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterCollectionData(by: searchText)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func filterCollectionData(by text: String?) {
+        if let searchText = text, !searchText.isEmpty {
+            self.isSearching = true
+            self.filteredCollectionData = collectionData?.filter({ return $0.name.lowercased().contains(searchText.lowercased())})
+            self.tableView.reloadData()
+        }else{
+            self.isSearching = false
+            self.filteredCollectionData?.removeAll()
+            self.tableView.reloadData()
+        }
+    }
     
 }
